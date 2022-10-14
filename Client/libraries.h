b890037d61fe71082526696a14515d07eb65296f
stupak_da <43846458+stupakdm@users.noreply.github.com>
2022-10-14 12:34:45 +0300

    Create libraries.h

---
+++ Client/libraries.h
@@ -0,0 +1,14 @@
+#ifndef LIBRARIES_H
+#define LIBRARIES_H
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "unistd.h"
+#include <stdio.h>
+#include "stdlib.h"
+#include "string.h"
+
+#endif // LIBRARIES_H
